c6502f0f4e1df3edda1f2b74300009137467726c
HP <HP@LAPTOP-SMC8HIAI>
2021-11-23 10:03:53 +0530

    suraj new jenkins

---
+++ testActitime1/src/test/java/testActitime1/TestActiTimeLogin.java
@@ -0,0 +1,52 @@
+package testActitime1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestActiTimeLogin {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setPath()
+	{
+		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+	}
+	
+	@BeforeMethod
+    public void setUp()
+    {
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.get("http://laptop-smc8hiai/login.do");
+		
+		
+    }
+	
+	@Test
+	public void loginTest() throws InterruptedException
+	
+	{
+		Reporter.log("suraj",true);
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		Thread.sleep(5000);
+		driver.findElement(By.id("loginButton")).click();
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.quit();
+		
+	}
+
+}
